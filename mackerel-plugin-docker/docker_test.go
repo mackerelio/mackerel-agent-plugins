5ceeda0b68e52d2de8112e93dccec25ff71ee6ad
Shinji Tanaka <shinji.tanaka@gmail.com>
2015-09-11 15:20:23 +0900

    update test case

--- mackerel-plugin-docker/docker_test.go
+++ mackerel-plugin-docker/docker_test.go
@@ -8,7 +8,7 @@ func TestGraphDefinition(t *testing.T) {
 	var docker DockerPlugin
 
 	graphdef := docker.GraphDefinition()
-	if len(graphdef) != 2 {
-		t.Errorf("GetTempfilename: %d should be 2", len(graphdef))
+	if len(graphdef) != 5 {
+		t.Errorf("GetTempfilename: %d should be 5", len(graphdef))
 	}
 }
