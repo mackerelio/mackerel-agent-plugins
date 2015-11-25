dbc51c009cda425ec1776c9d633db3fb4a96984e
linyows <linyows@gmail.com>
2015-11-26 00:19:35 +0900

    add test for FetchMetrics

--- mackerel-plugin-unicorn/unicorn_test.go
+++ mackerel-plugin-unicorn/unicorn_test.go
@@ -2,6 +2,15 @@ package main
 
 import "testing"
 
+func TestFetchMetrics(t *testing.T) {
+	var unicorn UnicornPlugin
+
+	stat, _ := unicorn.FetchMetrics()
+	if len(stat) != 5 {
+		t.Errorf("GetStat: %d should be 5", len(stat))
+	}
+}
+
 func TestGraphDefinition(t *testing.T) {
 	var unicorn UnicornPlugin
 
