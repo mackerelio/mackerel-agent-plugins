b8cca28419408ed4f4a7504187fb750c1b6f3349
Songmu <y.songmu@gmail.com>
2016-02-24 00:56:36 +0900

    add test for bsd

--- mackerel-plugin-uptime/uptime_bsd.go
+++ mackerel-plugin-uptime/uptime_bsd.go
@@ -19,11 +19,15 @@ func fetchMetrics() (map[string]interface{}, error) {
 	if err != nil {
 		return nil, fmt.Errorf("faild to fetch uptime: %s", err)
 	}
+	return calcMetrics(out.String(), time.Now().Unix())
+}
+
+func calcMetrics(str string, nowEpoch int64) (map[string]interface{}, error) {
 	// { sec = 1455448176, usec = 0 } Sun Feb 14 20:09:36 2016
-	cols := strings.Split(out.String(), " ")
-	epoch, err := strconv.ParseUint(strings.Trim(cols[3], ","), 10, 64)
+	cols := strings.Split(str, " ")
+	epoch, err := strconv.ParseInt(strings.Trim(cols[3], ","), 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("Faild to parse uptime: %s", err)
 	}
-	return map[string]interface{}{"seconds": float64(uint64(time.Now().Unix()) - epoch)}, nil
+	return map[string]interface{}{"seconds": float64(nowEpoch - epoch)}, nil
 }
---
+++ mackerel-plugin-uptime/uptime_bsd_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseMetrics(t *testing.T) {
+	r, err := calcMetrics("{ sec = 1455448176, usec = 0 } Sun Feb 14 20:09:36 2016", 1456242880)
+	if err != nil {
+		t.Errorf("error should be nil but: %s", err)
+	}
+	if !reflect.DeepEqual(r, map[string]interface{}{"seconds": float64(794704)}) {
+		t.Errorf("something went wrong. failed to parse uptime")
+	}
+}
