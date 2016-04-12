0386e8d1ef91ed7d8420ac02368d6fda795a638c
taku-k <taakuu19@gmail.com>
2016-04-12 17:39:45 +0900

    Add test for mackerel-plugin-proc-fd

--- mackerel-plugin-proc-fd/proc_fd.go
+++ mackerel-plugin-proc-fd/proc_fd.go
@@ -5,12 +5,9 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"strconv"
-	"strings"
 
 	mp "github.com/mackerelio/go-mackerel-plugin-helper"
 	"github.com/mackerelio/mackerel-agent/logging"
-	"github.com/mattn/go-pipeline"
 )
 
 var logger = logging.GetLogger("metrics.plugin.proc-fd")
@@ -24,7 +21,7 @@ type ProcfdPlugin struct {
 
 // FetchMetrics fetch the metrics
 func (p ProcfdPlugin) FetchMetrics() (map[string]interface{}, error) {
-	fds, err := p.getNumOpenFileDesc()
+	fds, err := openFd.getNumOpenFileDesc()
 	if err != nil {
 		return nil, err
 	}
@@ -56,44 +53,6 @@ func (p ProcfdPlugin) GraphDefinition() map[string](mp.Graphs) {
 	}
 }
 
-func (p ProcfdPlugin) getNumOpenFileDesc() (map[string]uint64, error) {
-	fds := make(map[string]uint64)
-
-	// Fetch all pids which contain specified process name
-	out, err := pipeline.Output(
-		[]string{"ps", "aux"},
-		[]string{"grep", p.Process},
-		[]string{"grep", "-v", "grep"},
-		[]string{"grep", "-v", "mackerel-plugin-proc-fd"},
-		[]string{"awk", "{print $2}"},
-	)
-	if err != nil {
-		// No matching with p.Process invokes this case
-		return nil, err
-	}
-
-	// List the number of all open files beloging to each pid
-	for _, pid := range strings.Split(strings.TrimSpace(string(out)), "\n") {
-		out, err = pipeline.Output(
-			[]string{"ls", "-l", fmt.Sprintf("/proc/%s/fd", pid)},
-			[]string{"grep", "-v", "total"},
-			[]string{"wc", "-l"},
-		)
-		if err != nil {
-			// The process with pid terminates"
-			return nil, err
-		}
-
-		num, err := strconv.ParseUint(strings.TrimSpace(string(out)), 10, 32)
-		if err != nil {
-			return nil, err
-		}
-		fds[pid] = num
-	}
-
-	return fds, nil
-}
-
 func normalizeForMetricName(process string) string {
 	// Mackerel accepts following characters in custom metric names
 	// [-a-zA-Z0-9_.]
@@ -114,6 +73,7 @@ func main() {
 
 	var fd ProcfdPlugin
 	fd.Process = *optProcess
+	openFd = RealOpenFd{fd.Process}
 	fd.NormalizedProcess = normalizeForMetricName(*optProcess)
 
 	helper := mp.NewMackerelPlugin(fd)
