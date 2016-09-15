c7b55d83336d7fba54b0fc8b8c3223e183159f65
湖山 翔平 <sion_cojp@yahoo.co.jp>
2016-09-15 12:20:37 +0900

    get data from cloud watch by using aws package

--- mackerel-plugin-aws-rds-aurora/aws-rds-aurora.go
+++ mackerel-plugin-aws-rds-aurora/aws-rds-aurora.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"time"
 
 	"github.com/crowdmob/goamz/aws"
+	"github.com/crowdmob/goamz/cloudwatch"
+	mp "github.com/mackerelio/go-mackerel-plugin"
 )
 
 // AuroraPlugin mackerel plugin for amazon Aurora
@@ -28,6 +32,41 @@ var metricsdefAurora = []string{
 	"InsertLatency", "SelectLatency", "UpdateLatency",
 	"CommitThroughput", "DDLThroughput", "DMLThroughput", "DeleteThroughput",
 	"InsertThroughput", "SelectThroughput", "UpdateThroughput",
+}
+
+func getLastPoint(cloudWatch *cloudwatch.CloudWatch, dimension *cloudwatch.Dimension, metricName string) (float64, error) {
+	now := time.Now()
+
+	response, err := cloudWatch.GetMetricStatistics(&cloudwatch.GetMetricStatisticsRequest{
+		Dimensions: []cloudwatch.Dimension{*dimension},
+		StartTime:  now.Add(time.Duration(180) * time.Second * -1), // 3 min (to fetch at least 1 data-point)
+		EndTime:    now,
+		MetricName: metricName,
+		Period:     60,
+		Statistics: []string{"Average"},
+		Namespace:  "AWS/RDS",
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	datapoints := response.GetMetricStatisticsResult.Datapoints
+	if len(datapoints) == 0 {
+		return 0, errors.New("fetched no datapoints")
+	}
+
+	latest := time.Unix(0, 0)
+	var latestVal float64
+	for _, dp := range datapoints {
+		if dp.Timestamp.Before(latest) {
+			continue
+		}
+
+		latest = dp.Timestamp
+		latestVal = dp.Average
+	}
+
+	return latestVal, nil
 }
 
 // GraphDefinition interface for mackerel plugin
