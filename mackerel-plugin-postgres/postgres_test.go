4249780779cc8712d29815ed5dbde0f595e17bd9
Songmu <y.songmu@gmail.com>
2016-10-20 21:39:09 +0900

    fix test

--- mackerel-plugin-postgres/postgres_test.go
+++ mackerel-plugin-postgres/postgres_test.go
@@ -13,11 +13,7 @@ func TestFetchStatDatabase(t *testing.T) {
 	columns := []string{"xact_commit", "xact_rollback", "blks_read", "blks_hit", "blk_read_time", "blk_write_time",
 		"tup_returned", "tup_fetched", "tup_inserted", "tup_updated", "tup_deleted", "deadlocks", "temp_bytes"}
 
-	testdb.StubQuery(`
-		select xact_commit, xact_rollback, blks_read, blks_hit, blk_read_time, blk_write_time,
-		tup_returned, tup_fetched, tup_inserted, tup_updated, tup_deleted, deadlocks, temp_bytes
-		from pg_stat_database
-	`, testdb.RowsFromCSVString(columns, `
+	testdb.StubQuery(`SELECT * FROM pg_stat_database`, testdb.RowsFromCSVString(columns, `
 	1,2,3,4,5,6,7,8,9,10,11,12,13
 	10,20,30,40,50,60,70,80,90,100,110,120,130
 	`))
@@ -25,9 +21,9 @@ func TestFetchStatDatabase(t *testing.T) {
 	stat, err := fetchStatDatabase(db)
 
 	expected := map[string]interface{}{
-		"xact_commit":  11.0,
-		"blks_hit":     44.0,
-		"tup_returned": 77.0,
+		"xact_commit":  uint64(11),
+		"blks_hit":     uint64(44),
+		"tup_returned": uint64(77),
 	}
 
 	if err != nil {
