1b4bd532e77f744b45eb291b8a3490486e521535
littlekbt <kr.kubota.11@gmail.com>
2017-02-13 10:46:28 +0900

    fix: path

--- mackerel-plugin-gcp-compute-engine/main.go
+++ mackerel-plugin-gcp-compute-engine/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/mackerelio/mackerel-agent-plugins/mackerel-plugn-gcp-compute-engine/lib"
+	"github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-gcp-compute-engine/lib"
 )
 
 func main() {
