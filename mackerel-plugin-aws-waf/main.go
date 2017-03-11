cb6f0e789016fc05f7c259c639d1d4d3f0e2f9ea
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2017-03-11 11:20:18 +0900

    Fix import path

--- mackerel-plugin-aws-waf/main.go
+++ mackerel-plugin-aws-waf/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/holidayworking/mackerel-plugin-aws-waf/lib"
+import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-aws-waf/lib"
 
 func main() {
 	mpawswaf.Do()
