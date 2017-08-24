29fe45088573462dbf24663413ae3f379388deb0
doublemarket <doublemarket@github.com>
2017-08-25 00:24:09 +0900

    Add mackerel-plugin-json

---
+++ mackerel-plugin-json/lib/json.go
@@ -0,0 +1,115 @@
+package mpjson
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// JSONPlugin plugin for JSON
+type JSONPlugin struct {
+	Target             string
+	Tempfile           string
+	URL                string
+	Prefix             string
+	InsecureSkipVerify bool
+	ShowOnlyNum        bool
+	ExcludeExp         *regexp.Regexp
+	IncludeExp         *regexp.Regexp
+}
+
+func (p JSONPlugin) traverseMap(content interface{}, path []string) (map[string]float64, error) {
+	stat := make(map[string]float64)
+	if reflect.TypeOf(content).Kind() == reflect.Slice {
+		for i, c := range content.([]interface{}) {
+			ts, _ := p.traverseMap(c, append(path, strconv.Itoa(i)))
+			for tk, tv := range ts {
+				stat[tk] = tv
+			}
+		}
+	} else {
+		for k, v := range content.(map[string]interface{}) {
+			if v != nil {
+				if reflect.TypeOf(v).Kind() == reflect.Map {
+					ts, _ := p.traverseMap(v, append(path, k))
+					for tk, tv := range ts {
+						stat[tk] = tv
+					}
+				} else if reflect.TypeOf(v).Kind() == reflect.Slice {
+					for i, c := range v.([]interface{}) {
+						ts, _ := p.traverseMap(c, append(path, strconv.Itoa(i)))
+						for tk, tv := range ts {
+							stat[tk] = tv
+						}
+					}
+				} else {
+					tk, tv := p.outputMetric(strings.Join(append(path, k), "."), v)
+					if tk != "" {
+						stat[tk] = tv
+					}
+				}
+			}
+		}
+	}
+
+	return stat, nil
+}
+
+func (p JSONPlugin) outputMetric(path string, value interface{}) (string, float64) {
+	if p.IncludeExp.MatchString(path) && !p.ExcludeExp.MatchString(path) {
+		if reflect.TypeOf(value).Kind() == reflect.Float64 {
+			return path, value.(float64)
+		}
+	}
+
+	return "", 0
+}
+
+// FetchMetrics interface for mackerel-plugin
+func (p JSONPlugin) FetchMetrics() (map[string]float64, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: p.InsecureSkipVerify},
+	}
+	client := &http.Client{Transport: tr}
+	response, _ := client.Get(p.URL)
+	bytes, _ := ioutil.ReadAll(response.Body)
+
+	var content interface{}
+	err := json.Unmarshal(bytes, &content)
+	if err != nil {
+		panic(err)
+	}
+
+	return p.traverseMap(content, []string{p.Prefix})
+}
+
+// Do do doo
+func Do() {
+	url := flag.String("url", "", "URL to get a JSON")
+	prefix := flag.String("prefix", "custom", "Prefix for metric names")
+	insecure := flag.Bool("insecure", false, "Skip certificate verifications")
+	exclude := flag.String("exclude", `^$`, "Exclude metrics that matches the expression")
+	include := flag.String("include", ``, "Output metrics that matches the expression")
+	flag.Parse()
+
+	var jsonplugin JSONPlugin
+
+	jsonplugin.URL = *url
+	jsonplugin.Prefix = *prefix
+	jsonplugin.InsecureSkipVerify = *insecure
+	jsonplugin.ExcludeExp = regexp.MustCompile(*exclude)
+	jsonplugin.IncludeExp = regexp.MustCompile(*include)
+
+	metrics, _ := jsonplugin.FetchMetrics()
+	for k, v := range metrics {
+		fmt.Printf("%s\t%f\t%d\n", k, v, time.Now().Unix())
+	}
+}
---
+++ mackerel-plugin-json/lib/json_test.go
@@ -0,0 +1,88 @@
+package mpjson
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTraverseMap(t *testing.T) {
+	var p JSONPlugin
+
+	p.Prefix = "testprefix"
+	p.ExcludeExp = regexp.MustCompile(`^$`)
+	p.IncludeExp = regexp.MustCompile(``)
+
+	bytes, _ := ioutil.ReadFile("test/jolokia.json")
+	var content interface{}
+	err := json.Unmarshal(bytes, &content)
+	if err != nil {
+		panic(err)
+	}
+	stat, err := p.traverseMap(content, []string{p.Prefix})
+
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, 1073741824, stat[p.Prefix+".value.HeapMemoryUsage.init"])
+
+	// A metric having null (nil) value shouldn't be contained.
+	if _, ok := stat[p.Prefix+".value.ObjectPendingFinalizationCount"]; ok {
+		fmt.Println(ok)
+		t.Fatalf(p.Prefix + ".value.ObjectPendingFinalizationCount shouldn't exist.")
+	}
+
+	// Tests for slice handling
+	// An object is a slice
+	bytes, _ = ioutil.ReadFile("test/array.json")
+	err = json.Unmarshal(bytes, &content)
+	if err != nil {
+		panic(err)
+	}
+	stat, err = p.traverseMap(content, []string{p.Prefix})
+
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, 1, stat[p.Prefix+".0.count1"])
+	assert.EqualValues(t, 3, stat[p.Prefix+".1.count1"])
+
+	// Slices with in an object
+	bytes, _ = ioutil.ReadFile("test/array_within.json")
+	err = json.Unmarshal(bytes, &content)
+	if err != nil {
+		panic(err)
+	}
+	stat, err = p.traverseMap(content, []string{p.Prefix})
+
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, 10, stat[p.Prefix+".0.count1"])
+	assert.EqualValues(t, 30, stat[p.Prefix+".1.count1"])
+}
+
+func TestOutputMetric(t *testing.T) {
+	var p JSONPlugin
+	p.Prefix = "testprefix"
+
+	p.ExcludeExp = regexp.MustCompile(`^$`)
+	p.IncludeExp = regexp.MustCompile(``)
+
+	// Should work if the value is float
+	path, value := p.outputMetric("hoge.fuga.foo", 12345.67)
+	assert.EqualValues(t, "hoge.fuga.foo", path)
+	assert.EqualValues(t, 12345.67, value)
+
+	// Should not work if the value is string
+	path, value = p.outputMetric("hoge.fuga.foo", "boo")
+	assert.EqualValues(t, "", path)
+	assert.EqualValues(t, 0, value)
+
+	// The output should be empty if ExcludeExp is specified and matches.
+	p.ExcludeExp = regexp.MustCompile(`h??e`)
+	path, value = p.outputMetric("hoge.fuga.foo", 12345.67)
+	assert.EqualValues(t, "", path)
+	assert.EqualValues(t, 0, value)
+}
