f17384500d0b513e1c8856fb1d8633fa3cf32948
shibayu36 <shibayu36@gmail.com>
2017-11-07 10:17:02 +0900

    Move aws-ec2 code to mackerelio/mackerel-plugin-aws-ec2

--- mackerel-plugin-aws-ec2/main.go
+++ mackerel-plugin-aws-ec2/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-aws-ec2/lib"
+import "github.com/mackerelio/mackerel-plugin-aws-ec2/lib"
 
 func main() {
 	mpawsec2.Do()
