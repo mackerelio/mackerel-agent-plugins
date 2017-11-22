b359efa091de7197d8fedca71bccb6ad1e55b765
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-11-22 15:15:03 +0900

    Do error handling

--- mackerel-plugin-json/lib/json.go
+++ mackerel-plugin-json/lib/json.go
@@ -80,13 +80,18 @@ func (p JSONPlugin) FetchMetrics() (map[string]float64, error) {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: p.InsecureSkipVerify},
 	}
 	client := &http.Client{Transport: tr}
-	response, _ := client.Get(p.URL)
-	bytes, _ := ioutil.ReadAll(response.Body)
+	response, err := client.Get(p.URL)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
 
 	var content interface{}
-	err := json.Unmarshal(bytes, &content)
-	if err != nil {
-		panic(err)
+	if err := json.Unmarshal(bytes, &content); err != nil {
+		return nil, err
 	}
 
 	return p.traverseMap(content, []string{p.Prefix})
