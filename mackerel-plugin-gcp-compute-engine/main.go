4739f10412008c72d0cd75572cec9ab5fe0a19ab
shibayu36 <shibayu36@gmail.com>
2018-01-05 12:16:04 +0900

    Move to mackerelio/mackerel-plugin-gcp-compute-engine

--- mackerel-plugin-gcp-compute-engine/main.go
+++ mackerel-plugin-gcp-compute-engine/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-gcp-compute-engine/lib"
+import "github.com/mackerelio/mackerel-plugin-gcp-compute-engine/lib"
 
 func main() {
 	mpgcpcomputeengine.Do()
