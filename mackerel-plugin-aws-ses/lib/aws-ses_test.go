6316f3326a2b97fca9fcb3cbc87528dbbda8003f
yseto <yseto@users.noreply.github.com>
2021-02-19 18:17:17 +0900

    added a invalid argument test

--- mackerel-plugin-aws-ses/lib/aws-ses_test.go
+++ mackerel-plugin-aws-ses/lib/aws-ses_test.go
@@ -30,3 +30,14 @@ func TestPrepare_InvalidEndpoint(t *testing.T) {
 		t.Errorf("Error actual = %v, and Expected = %v.", actual, expected)
 	}
 }
+
+func TestPrepare_InvalidEndpoint2(t *testing.T) {
+	var p SESPlugin
+	p.Endpoint = "https://email.us-west-2.foobar.bazqux.com"
+	actual := p.prepare()
+	expected := "--endpoint is invalid"
+
+	if actual.Error() != expected {
+		t.Errorf("Error actual = %v, and Expected = %v.", actual, expected)
+	}
+}
