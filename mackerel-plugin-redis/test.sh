02ddc35235c24d1ea1739aaab3f6549c70157108
lufia <lufia@lufia.org>
2021-06-17 14:21:23 +0900

    [ci][plugin-redis] check metrics

--- mackerel-plugin-redis/test.sh
+++ mackerel-plugin-redis/test.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-prog=$(basename $0)
+prog=$(basename "$0")
 if ! [ -S /var/run/docker.sock ]
 then
 	echo "$prog: there are no running docker" >&2
 	exit 2
 fi
 
-cd $(dirname $0)
+cd "$(dirname "$0")" || exit
 PATH=$(pwd):$PATH
-plugin=$(basename $(pwd))
-if ! which $plugin >/dev/null
+plugin=$(basename "$(pwd)")
+if ! which "$plugin" >/dev/null
 then
 	echo "$prog: $plugin is not installed" >&2
 	exit 2
 fi
 
 export REDIS_PASSWORD=passpass
-docker run --name test-$plugin -p 16379:6379 -d redis:6 --requirepass $REDIS_PASSWORD
-trap 'docker stop test-$plugin; docker rm test-$plugin; exit' EXIT
+image=redis:6
+port=16379
+
+docker run --name "test-$plugin" -p $port:6379 -d $image --requirepass $REDIS_PASSWORD
+trap 'docker stop test-$plugin; docker rm test-$plugin; exit' 1 2 3 15 EXIT
 sleep 10
 
-exec $plugin -port 16379
+# to store previous value to calculate a diff of metrics
+$plugin -port $port >/dev/null 2>&1
+sleep 1
+
+$plugin -port $port | graphite-metric-test -f rule.txt
