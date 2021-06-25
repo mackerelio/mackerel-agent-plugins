756ca59e066a46d8bbf28f84f3b2b03e8e0ade37
lufia <lufia@lufia.org>
2021-06-25 22:48:31 +0900

    [ci][plugin-redis] exit code

--- mackerel-plugin-redis/test.sh
+++ mackerel-plugin-redis/test.sh
@@ -21,7 +21,7 @@ image=redis:6
 port=16379
 
 docker run --name "test-$plugin" -p $port:6379 -d $image --requirepass $REDIS_PASSWORD
-trap 'docker stop test-$plugin; docker rm test-$plugin; exit' 1 2 3 15 EXIT
+trap 'docker stop test-$plugin; docker rm test-$plugin; exit 1' 1 2 3 15
 sleep 10
 
 # to store previous value to calculate a diff of metrics
@@ -29,3 +29,8 @@ $plugin -port $port >/dev/null 2>&1
 sleep 1
 
 $plugin -port $port | graphite-metric-test -f rule.txt
+status=$?
+
+docker stop "test-$plugin"
+docker rm "test-$plugin"
+exit $status
