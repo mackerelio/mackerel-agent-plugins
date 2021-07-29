b0e6a182b5dc1cc52a1014d68174e1a63cbbd796
lufia <lufia@lufia.org>
2021-07-29 21:05:22 +0900

    [accesslog] add TestSkipLogOnceIfNoPos

--- mackerel-plugin-accesslog/lib/accesslog_test.go
+++ mackerel-plugin-accesslog/lib/accesslog_test.go
@@ -1,6 +1,9 @@
 package mpaccesslog
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 
@@ -172,5 +175,39 @@ func TestFetchMetricsWithCustomParser(t *testing.T) {
 	}
 	if !reflect.DeepEqual(out, expected) {
 		t.Errorf("out:  %#v\n want: %#v", out, expected)
+	}
+}
+
+func TestSkipLogOnceIfNoPos(t *testing.T) {
+	dir := t.TempDir()
+	posFile := filepath.Join(dir, "plugin-accesslog.test.pos")
+	p := &AccesslogPlugin{
+		file:    "testdata/sample-ltsv.tsv",
+		posFile: posFile,
+	}
+	out, err := p.FetchMetrics()
+	if err != nil {
+		t.Errorf("error should be nil but: %+v", err)
+		return
+	}
+	if n := len(out); n != 0 {
+		t.Errorf("got %d metrics; but want 0", n)
+	}
+
+	// see https://github.com/Songmu/postailer/blob/master/postailer.go#L27-L30
+	var pos struct {
+		Pos int64 `json:"pos"`
+	}
+	b, err := os.ReadFile(posFile)
+	if err != nil {
+		t.Errorf("ReadFile(%s): %v", posFile, err)
+		return
+	}
+	if err := json.Unmarshal(b, &pos); err != nil {
+		t.Fatal(err)
+	}
+	var want int64 = 1247
+	if pos.Pos != want {
+		t.Errorf("saved position = %d; want %d", pos.Pos, want)
 	}
 }
