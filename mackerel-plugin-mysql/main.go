39b4749b79918512f8bdcad33181fbea2106e9b6
yseto <yseto@users.noreply.github.com>
2022-12-21 15:59:47 +0900

    remove mackerel-plugin-mysql
    moved https://github.com/mackerelio/mackerel-plugin-mysql

--- mackerel-plugin-mysql/main.go
+++ mackerel-plugin-mysql/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-mysql/lib"
+import "github.com/mackerelio/mackerel-plugin-mysql/lib"
 
 func main() {
 	mpmysql.Do()
