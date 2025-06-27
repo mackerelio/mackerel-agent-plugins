692bf65453f00011e7e822644d6e139665e6c512
yseto <yseto@users.noreply.github.com>
2025-06-27 18:09:36 +0900

    update aws-sdk-go-v2 on aws-ec2-ebs

--- go.mod
+++ go.mod
@@ -10,6 +10,12 @@ require (
 	github.com/Songmu/timeout v0.4.0
 	github.com/StackExchange/wmi v1.2.1
 	github.com/aws/aws-sdk-go v1.55.5
+	github.com/aws/aws-sdk-go-v2 v1.36.5
+	github.com/aws/aws-sdk-go-v2/config v1.29.17
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.70
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.32
+	github.com/aws/aws-sdk-go-v2/service/cloudwatch v1.45.3
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.227.0
 	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5
 	github.com/fsouza/go-dockerclient v1.9.6
 	github.com/fukata/golang-stats-api-handler v1.0.0
@@ -47,6 +53,15 @@ require (
 	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/Songmu/go-ltsv v0.0.0-20181014062614-c30af2b7b171 // indirect
 	github.com/Songmu/wrapcommander v0.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.36 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.36 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.34.0 // indirect
+	github.com/aws/smithy-go v1.22.4 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/containerd/containerd v1.6.26 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
--- mackerel-plugin-aws-ec2-ebs/lib/aws-ec2-ebs.go
+++ mackerel-plugin-aws-ec2-ebs/lib/aws-ec2-ebs.go
@@ -1,19 +1,25 @@
 package mpawsec2ebs
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"os/signal"
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cloudwatch"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
+	cloudwatchTypes "github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	mp "github.com/mackerelio/go-mackerel-plugin-helper"
 )
 
@@ -22,8 +28,8 @@ const (
 	aggregationPeriod   = 60
 )
 
-var metricPeriodByVolumeType = map[string]int{
-	"io1": 60,
+var metricPeriodByVolumeType = map[types.VolumeType]int{
+	types.VolumeTypeIo1: 60,
 }
 
 var baseGraphs = []string{
@@ -46,13 +52,13 @@ var io1Graphs = append([]string{
 
 type additionalCloudWatchSetting struct {
 	MetricName string
-	Statistics string
+	Statistics cloudwatchTypes.Statistic
 	CalcFunc   func(float64, float64) float64
 }
 
 type cloudWatchSetting struct {
 	MetricName string
-	Statistics string
+	Statistics cloudwatchTypes.Statistic
 	CalcFunc   func(float64) float64
 	Additional *additionalCloudWatchSetting
 }
@@ -76,85 +82,85 @@ func valPerOps(val, ops float64) float64 {
 // http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/monitoring-volume-status.html
 var cloudwatchdefs = map[string](cloudWatchSetting){
 	"ec2.ebs.bandwidth.#.read": cloudWatchSetting{
-		MetricName: "VolumeReadBytes", Statistics: "Sum",
+		MetricName: "VolumeReadBytes", Statistics: cloudwatchTypes.StatisticSum,
 		CalcFunc: valuePerSec,
 	},
 	"ec2.ebs.bandwidth.#.write": cloudWatchSetting{
-		MetricName: "VolumeWriteBytes", Statistics: "Sum",
+		MetricName: "VolumeWriteBytes", Statistics: cloudwatchTypes.StatisticSum,
 		CalcFunc: valuePerSec,
 	},
 	"ec2.ebs.throughput.#.read": cloudWatchSetting{
-		MetricName: "VolumeReadOps", Statistics: "Sum",
+		MetricName: "VolumeReadOps", Statistics: cloudwatchTypes.StatisticSum,
 		CalcFunc: valuePerSec,
 	},
 	"ec2.ebs.throughput.#.write": cloudWatchSetting{
-		MetricName: "VolumeWriteOps", Statistics: "Sum",
+		MetricName: "VolumeWriteOps", Statistics: cloudwatchTypes.StatisticSum,
 		CalcFunc: valuePerSec,
 	},
 	"ec2.ebs.size_per_op.#.read": cloudWatchSetting{
-		MetricName: "VolumeReadBytes", Statistics: "Average",
+		MetricName: "VolumeReadBytes", Statistics: cloudwatchTypes.StatisticAverage,
 		CalcFunc: value,
 	},
 	"ec2.ebs.size_per_op.#.write": cloudWatchSetting{
-		MetricName: "VolumeWriteBytes", Statistics: "Average",
+		MetricName: "VolumeWriteBytes", Statistics: cloudwatchTypes.StatisticAverage,
 		CalcFunc: value,
 	},
 	"ec2.ebs.latency.#.read": cloudWatchSetting{
-		MetricName: "VolumeTotalReadTime", Statistics: "Average",
+		MetricName: "VolumeTotalReadTime", Statistics: cloudwatchTypes.StatisticAverage,
 		CalcFunc: sec2msec,
 	},
 	"ec2.ebs.latency.#.write": cloudWatchSetting{
-		MetricName: "VolumeTotalWriteTime", Statistics: "Average",
+		MetricName: "VolumeTotalWriteTime", Statistics: cloudwatchTypes.StatisticAverage,
 		CalcFunc: sec2msec,
 	},
 	"ec2.ebs.queue_length.#.queue_length": cloudWatchSetting{
-		MetricName: "VolumeQueueLength", Statistics: "Average",
+		MetricName: "VolumeQueueLength", Statistics: cloudwatchTypes.StatisticAverage,
 		CalcFunc: value,
 	},
 	"ec2.ebs.idle_time.#.idle_time": cloudWatchSetting{
-		MetricName: "VolumeIdleTime", Statistics: "Sum",
+		MetricName: "VolumeIdleTime", Statistics: cloudwatchTypes.StatisticSum,
 		CalcFunc: func(val float64) float64 { return val / aggregationPeriod * 100.0 },
 	},
 	"ec2.ebs.throughput_delivered.#.throughput_delivered": cloudWatchSetting{
-		MetricName: "VolumeThroughputPercentage", Statistics: "Average",
+		MetricName: "VolumeThroughputPercentage", Statistics: cloudwatchTypes.StatisticAverage,
 		CalcFunc: value,
 	},
 	"ec2.ebs.consumed_ops.#.consumed_ops": cloudWatchSetting{
-		MetricName: "VolumeConsumedReadWriteOps", Statistics: "Sum",
+		MetricName: "VolumeConsumedReadWriteOps", Statistics: cloudwatchTypes.StatisticSum,
 		CalcFunc: value,
 	},
 	"ec2.ebs.burst_balance.#.burst_balance": cloudWatchSetting{
-		MetricName: "BurstBalance", Statistics: "Average",
+		MetricName: "BurstBalance", Statistics: cloudwatchTypes.StatisticAverage,
 		CalcFunc: value,
 	},
 }
 
 var cloudwatchdefsNitro = map[string](cloudWatchSetting){
 	"ec2.ebs.size_per_op.#.read": cloudWatchSetting{
-		MetricName: "VolumeReadBytes", Statistics: "Sum",
+		MetricName: "VolumeReadBytes", Statistics: cloudwatchTypes.StatisticSum,
 		Additional: &additionalCloudWatchSetting{
-			MetricName: "VolumeReadOps", Statistics: "Sum",
+			MetricName: "VolumeReadOps", Statistics: cloudwatchTypes.StatisticSum,
 			CalcFunc: valPerOps,
 		},
 	},
 	"ec2.ebs.size_per_op.#.write": cloudWatchSetting{
-		MetricName: "VolumeWriteBytes", Statistics: "Sum",
+		MetricName: "VolumeWriteBytes", Statistics: cloudwatchTypes.StatisticSum,
 		Additional: &additionalCloudWatchSetting{
-			MetricName: "VolumeWriteOps", Statistics: "Sum",
+			MetricName: "VolumeWriteOps", Statistics: cloudwatchTypes.StatisticSum,
 			CalcFunc: valPerOps,
 		},
 	},
 	"ec2.ebs.latency.#.read": cloudWatchSetting{
-		MetricName: "VolumeTotalReadTime", Statistics: "Sum",
+		MetricName: "VolumeTotalReadTime", Statistics: cloudwatchTypes.StatisticSum,
 		Additional: &additionalCloudWatchSetting{
-			MetricName: "VolumeReadOps", Statistics: "Sum",
+			MetricName: "VolumeReadOps", Statistics: cloudwatchTypes.StatisticSum,
 			CalcFunc: valPerOps,
 		},
 	},
 	"ec2.ebs.latency.#.write": cloudWatchSetting{
-		MetricName: "VolumeTotalWriteTime", Statistics: "Sum",
+		MetricName: "VolumeTotalWriteTime", Statistics: cloudwatchTypes.StatisticSum,
 		Additional: &additionalCloudWatchSetting{
-			MetricName: "VolumeWriteOps", Statistics: "Sum",
+			MetricName: "VolumeWriteOps", Statistics: cloudwatchTypes.StatisticSum,
 			CalcFunc: valPerOps,
 		},
 	},
@@ -239,30 +245,31 @@ type EBSPlugin struct {
 	InstanceID      string
 
 	// internal states
-	Credentials *credentials.Credentials
-	EC2         *ec2.EC2
-	CloudWatch  *cloudwatch.CloudWatch
-	Volumes     []*ec2.Volume
-	Hypervisor  string
-	Session     *session.Session
+	EC2        *ec2.Client
+	CloudWatch *cloudwatch.Client
+	Volumes    []types.Volume
+	Hypervisor types.InstanceTypeHypervisor
 }
 
-func (p *EBSPlugin) prepare() error {
+func (p *EBSPlugin) prepare(ctx context.Context) error {
+	var opts []func(*config.LoadOptions) error
 	if p.AccessKeyID != "" && p.SecretAccessKey != "" {
-		p.Credentials = credentials.NewStaticCredentials(p.AccessKeyID, p.SecretAccessKey, "")
+		opts = append(opts, config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(p.AccessKeyID, p.SecretAccessKey, "")))
+	}
+	if p.Region != "" {
+		opts = append(opts, config.WithRegion(p.Region))
 	}
 
-	var err error
-	p.Session, err = session.NewSession(&aws.Config{Credentials: p.Credentials, Region: &p.Region})
+	cfg, err := config.LoadDefaultConfig(ctx, opts...)
 	if err != nil {
 		return err
 	}
 
-	p.EC2 = ec2.New(p.Session)
+	p.EC2 = ec2.NewFromConfig(cfg)
 
-	var instanceType string
-	instance, err := p.EC2.DescribeInstances(&ec2.DescribeInstancesInput{
-		InstanceIds: []*string{&p.InstanceID},
+	var instanceType types.InstanceType
+	instance, err := p.EC2.DescribeInstances(ctx, &ec2.DescribeInstancesInput{
+		InstanceIds: []string{p.InstanceID},
 	})
 	if err != nil {
 		return err
@@ -272,12 +279,12 @@ func (p *EBSPlugin) prepare() error {
 	}
 	for i := range instance.Reservations {
 		for j := range instance.Reservations[i].Instances {
-			instanceType = *instance.Reservations[i].Instances[j].InstanceType
+			instanceType = instance.Reservations[i].Instances[j].InstanceType
 		}
 	}
 
-	instanceDetail, err := p.EC2.DescribeInstanceTypes(&ec2.DescribeInstanceTypesInput{
-		InstanceTypes: []*string{&instanceType},
+	instanceDetail, err := p.EC2.DescribeInstanceTypes(ctx, &ec2.DescribeInstanceTypesInput{
+		InstanceTypes: []types.InstanceType{instanceType},
 	})
 	if err != nil {
 		return err
@@ -286,16 +293,14 @@ func (p *EBSPlugin) prepare() error {
 		return errors.New("DescribeInstanceTypes response has NextToken")
 	}
 	for i := range instanceDetail.InstanceTypes {
-		p.Hypervisor = *instanceDetail.InstanceTypes[i].Hypervisor
+		p.Hypervisor = instanceDetail.InstanceTypes[i].Hypervisor
 	}
 
-	resp, err := p.EC2.DescribeVolumes(&ec2.DescribeVolumesInput{
-		Filters: []*ec2.Filter{
+	resp, err := p.EC2.DescribeVolumes(ctx, &ec2.DescribeVolumesInput{
+		Filters: []types.Filter{
 			{
-				Name: aws.String("attachment.instance-id"),
-				Values: []*string{
-					&p.InstanceID,
-				},
+				Name:   aws.String("attachment.instance-id"),
+				Values: []string{p.InstanceID},
 			},
 		},
 	})
@@ -311,22 +316,24 @@ func (p *EBSPlugin) prepare() error {
 		return errors.New("DescribeVolumes response has no volumes")
 	}
 
+	p.CloudWatch = cloudwatch.NewFromConfig(cfg)
+
 	return nil
 }
 
 var errNoDataPoint = errors.New("fetched no datapoints")
 
-func (p EBSPlugin) getLastPoint(vol *ec2.Volume, metricName string, statType string) (float64, error) {
+func (p EBSPlugin) getLastPoint(ctx context.Context, vol types.Volume, metricName string, statType cloudwatchTypes.Statistic) (float64, error) {
 	now := time.Now()
 
 	period := metricPeriodDefault
-	if tmp, ok := metricPeriodByVolumeType[*vol.VolumeType]; ok {
+	if tmp, ok := metricPeriodByVolumeType[vol.VolumeType]; ok {
 		period = tmp
 	}
 	start := now.Add(time.Duration(period) * 3 * time.Second * -1)
 
-	resp, err := p.CloudWatch.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
-		Dimensions: []*cloudwatch.Dimension{
+	resp, err := p.CloudWatch.GetMetricStatistics(ctx, &cloudwatch.GetMetricStatisticsInput{
+		Dimensions: []cloudwatchTypes.Dimension{
 			{
 				Name:  aws.String("VolumeId"),
 				Value: vol.VolumeId,
@@ -335,8 +342,8 @@ func (p EBSPlugin) getLastPoint(vol *ec2.Volume, metricName string, statType str
 		StartTime:  &start,
 		EndTime:    &now,
 		MetricName: &metricName,
-		Period:     aws.Int64(aggregationPeriod),
-		Statistics: []*string{&statType},
+		Period:     aws.Int32(aggregationPeriod),
+		Statistics: []cloudwatchTypes.Statistic{statType},
 		Namespace:  aws.String("AWS/EBS"),
 	})
 	if err != nil {
@@ -367,8 +374,8 @@ func (p EBSPlugin) getLastPoint(vol *ec2.Volume, metricName string, statType str
 	return latestVal, nil
 }
 
-func (p EBSPlugin) fetch(volume *ec2.Volume, setting cloudWatchSetting) (float64, error) {
-	val, err := p.getLastPoint(volume, setting.MetricName, setting.Statistics)
+func (p EBSPlugin) fetch(ctx context.Context, volume types.Volume, setting cloudWatchSetting) (float64, error) {
+	val, err := p.getLastPoint(ctx, volume, setting.MetricName, setting.Statistics)
 	if err != nil {
 		return 0, fmt.Errorf("%s %w : %s", *volume.VolumeId, err, setting.MetricName)
 	}
@@ -377,7 +384,7 @@ func (p EBSPlugin) fetch(volume *ec2.Volume, setting cloudWatchSetting) (float64
 		return setting.CalcFunc(val), nil
 	}
 
-	val2, err := p.getLastPoint(volume, setting.Additional.MetricName, setting.Additional.Statistics)
+	val2, err := p.getLastPoint(ctx, volume, setting.Additional.MetricName, setting.Additional.Statistics)
 	if err != nil {
 		return 0, fmt.Errorf("%s %w : %s", *volume.VolumeId, err, setting.Additional.MetricName)
 	}
@@ -389,17 +396,16 @@ func (p EBSPlugin) FetchMetrics() (map[string]interface{}, error) {
 	stat := make(map[string]interface{})
 
 	// Override when Nitro instance.
-	if p.Hypervisor == "nitro" {
+	if p.Hypervisor == types.InstanceTypeHypervisorNitro {
 		for i := range cloudwatchdefsNitro {
 			cloudwatchdefs[i] = cloudwatchdefsNitro[i]
 		}
 	}
 
-	p.CloudWatch = cloudwatch.New(p.Session)
 	for _, vol := range p.Volumes {
 		volumeID := normalizeVolumeID(*vol.VolumeId)
 		var graphs []string
-		if *vol.VolumeType == "io1" {
+		if vol.VolumeType == types.VolumeTypeIo1 {
 			graphs = io1Graphs
 		} else {
 			graphs = defaultGraphs
@@ -408,7 +414,7 @@ func (p EBSPlugin) FetchMetrics() (map[string]interface{}, error) {
 			for _, metric := range graphdef[graphName].Metrics {
 				metricKey := graphName + "." + metric.Name
 				cloudwatchdef := cloudwatchdefs[metricKey]
-				val, err := p.fetch(vol, cloudwatchdef)
+				val, err := p.fetch(context.TODO(), vol, cloudwatchdef)
 				if err != nil {
 					if errors.Is(err, errNoDataPoint) {
 						// nop
@@ -433,6 +439,9 @@ func normalizeVolumeID(volumeID string) string {
 	return strings.Replace(volumeID, ".", "_", -1)
 }
 
+// overwritten with syscall.SIGTERM on unix environment (see aws-ec2-ebs_unix.go)
+var defaultSignal = os.Interrupt
+
 // Do the plugin
 func Do() {
 	optRegion := flag.String("region", "", "AWS Region")
@@ -442,28 +451,40 @@ func Do() {
 	optTempfile := flag.String("tempfile", "", "Temp file name")
 	flag.Parse()
 
+	ctx, stop := signal.NotifyContext(context.Background(), defaultSignal)
+	defer stop()
+
 	var ebs EBSPlugin
 
 	ebs.Region = *optRegion
 	ebs.InstanceID = *optInstanceID
 
-	sess, err := session.NewSession()
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	// get metadata in ec2 instance
-	ec2MC := ec2metadata.New(sess)
+	imdsClient := imds.NewFromConfig(cfg)
 	if *optRegion == "" {
-		ebs.Region, _ = ec2MC.Region()
+		out, _ := imdsClient.GetRegion(ctx, nil)
+		ebs.Region = out.Region
 	}
 	if *optInstanceID == "" {
-		ebs.InstanceID, _ = ec2MC.GetMetadata("instance-id")
+		metadata, err := imdsClient.GetMetadata(ctx, &imds.GetMetadataInput{
+			Path: "instance-id",
+		})
+		if err != nil {
+			log.Fatalln(err)
+		}
+		content, _ := io.ReadAll(metadata.Content)
+		ebs.InstanceID = string(content)
 	}
 
 	ebs.AccessKeyID = *optAccessKeyID
 	ebs.SecretAccessKey = *optSecretAccessKey
 
-	if err := ebs.prepare(); err != nil {
+	if err := ebs.prepare(ctx); err != nil {
 		log.Fatalln(err)
 	}
 
