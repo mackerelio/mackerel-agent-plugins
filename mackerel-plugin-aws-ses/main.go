7c32e9188590219ad6e3cccd08c7995da4ef8fb8
yseto <yseto@users.noreply.github.com>
2025-07-09 19:53:45 +0900

    remove mackerel-plugin-aws-ses

--- mackerel-plugin-aws-ses/main.go
+++ mackerel-plugin-aws-ses/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-aws-ses/lib"
+import "github.com/mackerelio/mackerel-plugin-aws-ses/lib"
 
 func main() {
 	mpawsses.Do()
