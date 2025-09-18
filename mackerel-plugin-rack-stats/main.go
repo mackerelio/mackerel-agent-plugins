a0530311055924e1fd5b20bc28dd98f78d05b196
yseto <yseto@users.noreply.github.com>
2025-09-18 19:06:21 +0900

    rewrite files not included in the package

--- mackerel-plugin-rack-stats/main.go
+++ mackerel-plugin-rack-stats/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/mackerelio/mackerel-agent-plugins/mackerel-plugin-rack-stats/lib"
+import "github.com/mackerelio/mackerel-plugin-rack-stats/lib"
 
 func main() {
 	mprackstats.Do()
